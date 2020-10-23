b32f6f6a294871e3d6fa21a2f30366a036bfc20e
田端佑也 <tabayu5@gmail.com>
2020-10-23 22:46:30 +0900

    all at once

---
+++ 3-5.py
@@ -0,0 +1,3 @@
+with open('some.txt', 'w') as f:
+    f.write('some text')
+    f.close
---
+++ 4-1.py
@@ -0,0 +1,8 @@
+d = {'a':1, 'b':2, 'c':3}
+print(d.get('a'))
+
+n = None
+if n is None:
+    print('Yes None')
+else:
+    print('not None')
